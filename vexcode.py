a24a0f3b11b75ebf1fcd588dbfdd88a19ad0a0cf
ConnorP898 <c.paniura.31415926@gmail.com>
2025-05-02 08:46:39 -0600

    Update vexcode.py

--- vexcode.py
+++ vexcode.py
@@ -18,6 +18,7 @@ def when_started1():
 
     # block 1 #
 drivetrain.drive_for(FORWARD, 500, MM)
+drivetrain.set_drive_velocity(30, PERCENT)
 motor_3.spin(FORWARD)
 motor_3.set_stopping(HOLD)
 wait(1, SECONDS)
@@ -32,13 +33,13 @@ drivetrain.drive_for(FORWARD, 830, MM)
 motor_3.spin(FORWARD)
 wait(1, SECONDS)
 
-drivetrain.turn_for(RIGHT, 180, DEGREES)
-drivetrain.drive_for(FORWARD, 1130, MM)
-motor_2.spin_for(FORWARD, 100, DEGREES)
+motor_2.spin_for(FORWARD, 0, DEGREES)
 motor_2.set_stopping(HOLD)
+drivetrain.turn_for(RIGHT, 180, DEGREES)
+drivetrain.drive_for(FORWARD, 1120, MM)
 motor_3.spin(REVERSE)
 drivetrain.drive_for(REVERSE, 300, MM)
-motor_2.spin_for(REVERSE, 100, DEGREES)
+motor_2.spin_for(REVERSE, 90, DEGREES)
     # block 3 #
 drivetrain.turn_for(RIGHT, 180, DEGREES)
 drivetrain.drive_for(FORWARD, 860, MM)
